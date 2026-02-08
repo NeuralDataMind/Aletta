489f6b8102b5ca53b059cdb74411023bc19c014c
Mallikarjun Reddy Bardipuram <sunnygalaxym12@gmail.com>
2026-02-08 15:07:09 +0530

    modifed database

--- aletta_V.0.1/backend/app/core/database.py
+++ aletta_V.0.1/backend/app/core/database.py
@@ -2,17 +2,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Create the data directory if it doesn't exist
 if not os.path.exists("./data"):
     os.makedirs
 
 # Currently using the SQLite for initial setup later i will switch PgSQL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/aletta.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data/aletta.db")
+connect_args = {}
+if "sqlite" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args=connect_args
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
